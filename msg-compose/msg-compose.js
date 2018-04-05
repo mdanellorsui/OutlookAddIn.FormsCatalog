23e251ba017cde66dbcc97ade6173ef27d2eb91a
Mark Danello <mdanello@rsui.com>
2018-04-05 09:52:06 -0400

    JS code work

--- msg-compose/msg-compose.js
+++ msg-compose/msg-compose.js
@@ -10,17 +10,35 @@
   // The initialize function must be run each time a new page is loaded
   Office.initialize = function (reason) {
     $(document).ready(function () {
-      $('#run').click(run);
+    //  $('#run').click(run);
+
+      // Office.context.mailbox.item.notificationMessages.replaceAsync("progress1", {
+      //   type: "errorMessage",
+      //   message : "Foo Test"
+      //   });
+      document.getElementById("insert-button").value= "Text Chgd";
+      //$("#insert-button").style="background:red";  
+
+      //$('#insert-button').on('click', insertSelectedForms);
+
+
     });
   };
 
   function run() {
-    
-    
     /**
      * Insert your Outlook code here
      */
-    
   }
+
+  function insertSelectedForms() {
+    Office.context.mailbox.item.notificationMessages.replaceAsync("addin-message", {
+      type: "informationalMessage",
+      message: "Insert forms button was pressed",
+      icon : "iconid",
+      persistent: false
+    });
+  }
+
 
 })();
